d83664a9630a08411fa3311e3e932f75cc03cdbf
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-03-28 17:10:58 +0200

    Modified to choose PointsStrategy depending on karma status

--- app/src/main/java/com/example/myapplication/domain/score/Score.java
+++ app/src/main/java/com/example/myapplication/domain/score/Score.java
@@ -1,9 +1,11 @@
 package com.example.myapplication.domain.score;
 
 import com.example.myapplication.domain.Status;
+import com.example.myapplication.domain.score.points.KarmaBasedPointsStrategy;
 import com.example.myapplication.domain.score.points.PointsStrategy;
 import com.example.myapplication.domain.score.points.SimplePointsStrategy;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -15,8 +17,8 @@ public class Score implements Comparable<Score> {
     private static final int STARTING_KARMA = 0;
 
     private final AtomicInteger points;
+    private final AtomicBoolean isKarmaLocked;
     private final AtomicInteger karma;
-    private final PointsStrategy pointsStrategy;
 
     public Score() {
         this(STARTING_POINTS, STARTING_KARMA);
@@ -25,7 +27,7 @@ public class Score implements Comparable<Score> {
     public Score(int points, int karma) {
         this.points = new AtomicInteger(points);
         this.karma = new AtomicInteger(karma);
-        this.pointsStrategy = new SimplePointsStrategy();
+        this.isKarmaLocked = new AtomicBoolean(false);
     }
 
     public int getPoints() {
@@ -41,10 +43,18 @@ public class Score implements Comparable<Score> {
     }
 
     public void updateStatus(Status status) {
+        PointsStrategy pointsStrategy = isKarmaLocked.get() ? new SimplePointsStrategy() : new KarmaBasedPointsStrategy();
         int increment = pointsStrategy.computeIncrement(status, getKarma());
         int result = increment + getPoints() < 0 ? STARTING_POINTS : increment;
         points.set(result);
-        karma.set(getKarma() + 1);
+    }
+
+    public boolean setKarma(boolean locked) {
+        return isKarmaLocked.getAndSet(!locked);
+    }
+
+    public int incrementKarma(int increment) {
+        return karma.getAndSet(getKarma() + increment);
     }
 
     @Override
