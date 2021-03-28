930c7e13e8c51889d07aef0f0d158c4af3e7d417
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-03-28 16:46:10 +0200

    Added javadocs for ScoreboardService

--- app/src/main/java/com/example/myapplication/application/ScoreboardService.java
+++ app/src/main/java/com/example/myapplication/application/ScoreboardService.java
@@ -4,10 +4,23 @@ import com.example.myapplication.domain.score.Score;
 
 import java.util.List;
 
+/**
+ * Application-level service to manage the scoreboard.
+ */
 public interface ScoreboardService {
 
+    /**
+     * Finds the {@code amount} highest scores.
+     * @param amount top scores amount to be found
+     * @return the {@code amount} highest scores
+     */
     List<Score> findByHighestScore(int amount);
 
+    /**
+     * Adds a new score to the scoreboard.
+     * @param score a score to be added
+     * @return the scoreboard added score
+     */
     Score addScoreToScoreboard(Score score);
 
 }
