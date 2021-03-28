e9bf3c0f70442cafc7f43db37f4092fe01ce0368
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-03-28 16:38:09 +0200

    Added intention-revealing creation methods

--- app/src/main/java/com/example/myapplication/domain/countdown/Countdown.java
+++ app/src/main/java/com/example/myapplication/domain/countdown/Countdown.java
@@ -2,14 +2,29 @@ package com.example.myapplication.domain.countdown;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * A thread-safe countdown counter implementation.
+ */
 public class Countdown {
+
+    public static Countdown newDefault() {
+        return new Countdown();
+    }
+
+    public static Countdown newStartingFrom(int startFrom) {
+        return new Countdown(startFrom);
+    }
 
     private static final int START_FROM = 30;
 
     private final AtomicInteger counter;
 
     public Countdown() {
-        this.counter = new AtomicInteger(START_FROM);
+        this(START_FROM);
+    }
+
+    public Countdown(int startFrom) {
+        this.counter = new AtomicInteger(startFrom);
     }
 
     public boolean finishedCount() {
