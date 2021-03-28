bf2536add336c31081ea6110bee00b335cfb495a
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-03-28 16:57:37 +0200

    Refactored point strategies and added karma-based computation

--- app/src/main/java/com/example/myapplication/domain/score/points/KarmaBasedPointsStrategy.java
+++ app/src/main/java/com/example/myapplication/domain/score/points/KarmaBasedPointsStrategy.java
@@ -4,20 +4,20 @@ import com.example.myapplication.domain.Status;
 import com.example.myapplication.domain.random.RandomUtils;
 
 /**
- * A simple {@link PointsStrategy} which is the default points calculator.
+ * A karma-based {@link PointsStrategy} which is takes into account the current karma.
  */
-public class SimplePointsStrategy implements PointsStrategy {
+public class KarmaBasedPointsStrategy implements PointsStrategy {
 
     @Override
     public int computeIncrement(Status status, int currentKarma) {
         int increment;
         if(status == Status.SUCCESS) {
-            int min = 10;
-            int max = 50;
+            int min = 10 + (currentKarma / 2);
+            int max = 50 + (currentKarma / 2);
             increment = RandomUtils.randomInRange(min, max);
         } else {
-            int min = 5;
-            int max = 25;
+            int min = 5 + currentKarma;
+            int max = 25 + currentKarma;
             increment = -RandomUtils.randomInRange(min, max);
         }
         return increment;
