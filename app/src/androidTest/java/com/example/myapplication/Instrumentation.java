143673d53cdd281a259a72937a675ed1374922b4
JoseLuis <jolutoher18@gmail.com>
2021-04-17 17:08:32 +0200

    Setting up Android Studio for Cucumber Tests.

---
+++ app/src/androidTest/java/com/example/myapplication/Instrumentation.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+public class Instrumentation extends MonitoringInstrumentation {
+    private final CucumberInstrumentationCore instrumentationCore = new CucumberInstrumentationCore(this);         @Override
+    public void onCreate(Bundle arguments) {
+        super.onCreate(arguments);
+
+        instrumentationCore.create(arguments);
+        start();
+    }         @Override
+    public void onStart() {
+        super.onStart();
+
+        waitForIdleSync();
+        instrumentationCore.start();
+    }
+}
