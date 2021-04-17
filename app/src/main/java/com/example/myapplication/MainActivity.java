9db1aa1b0475a61bd7231f3144f3c41a36cc28ad
jolutoher18 <61881281+jolutoher18@users.noreply.github.com>
2021-04-17 13:54:11 +0200

    Update Project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
