d665d74e4ef03f5e141c6da05fd7785db37b533b
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-21 10:00:49 +0200

    Fixed score bonus calculator

--- app/src/main/java/com/example/myapplication/domain/score/ScoreBonusCalculator.java
+++ app/src/main/java/com/example/myapplication/domain/score/ScoreBonusCalculator.java
@@ -1,4 +1,11 @@
 package com.example.myapplication.domain.score;
 
 public class ScoreBonusCalculator {
+
+    public void addBonus(Score score) {
+        if(score.getPoints() >= 100 && score.getPoints() <= 200) {
+            score.addPoints(100);
+        }
+    }
+
 }
--- app/src/test/java/com/example/myapplication/domain/score/ScoreBonusCalculatorTest.java
+++ app/src/test/java/com/example/myapplication/domain/score/ScoreBonusCalculatorTest.java
@@ -1,4 +1,61 @@
 package com.example.myapplication.domain.score;
 
-public class ScoreCalculatorTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Acierto: (+) 10-50
+ * Fallo:   (-) 5-25
+ *
+ *
+ * Dada una puntuacion aplicarle el bonus y sumarle mas puntos
+ *
+ * Puntos     Incremento
+ * 100-200        100
+ * 200-300        200
+ * 400-500        500
+ */
+public class ScoreBonusCalculatorTest {
+
+    @Test
+    public void addNonApplicableBonusToScoreTest() {
+        // Arrange : arrangement of the preconditions of the test
+        Score score = new Score();
+        score.addPoints(5);
+        ScoreBonusCalculator calculator = new ScoreBonusCalculator();
+
+        // Act : the performance of the act that is being tested
+        calculator.addBonus(score);
+
+        // Assert : the assertion that the expected behavior occurred
+        Assert.assertEquals(5, score.getPoints());
+    }
+
+    @Test
+    public void addFirstApplicableBonusToScoreTest() {
+        // Arrange : arrangement of the preconditions of the test
+        Score score = new Score();
+        score.addPoints(125);
+        ScoreBonusCalculator calculator = new ScoreBonusCalculator();
+
+        // Act : the performance of the act that is being tested
+        calculator.addBonus(score);
+
+        // Assert : the assertion that the expected behavior occurred
+        Assert.assertEquals(225, score.getPoints());
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
